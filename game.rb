428728096ccb96b085a05e7cee6e7f2456b682c9
Marcus Larsson <marcus.larsson@elev.it-gymnasiet.se>
2012-11-23 10:23:59 +0100

    Added player class & sprite

--- game.rb
+++ game.rb
@@ -6,7 +6,19 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		@player = Player.create
 	end	
 end	
+
+class Player < Chingu::GameObject
+
+	#meta.contructor
+	def setup
+		@x, @y, = 350, 400
+		@image = Gosu::Image["alienblaster.png"]
+
+	end
+
+end
 
 Game.new.show
