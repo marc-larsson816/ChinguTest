2f446c3732158bcf4736e2e0a89fc41c4355ed87
Marcus Larsson <marcus.larsson@elev.it-gymnasiet.se>
2012-11-23 10:42:01 +0100

    Added movement to player class

--- game.rb
+++ game.rb
@@ -16,9 +16,37 @@ class Player < Chingu::GameObject
 	def setup
 		@x, @y, = 350, 400
 		@image = Gosu::Image["alienblaster.png"]
-
+		self.input = {
+			holding_left: :left,
+			holding_right: :right,
+			holding_up: :up,
+			holding_down: :down
+		}
 	end
 
+		def left
+			unless @x - 28 <= 0
+				@x -= 3
+			end
+		end
+
+		def right
+			unless @x + 28 >= 800
+				@x += 3
+			end
+		end
+
+		def up
+			unless @y - 28 <= 0
+			@y -=3
+			end
+		end
+
+		def down
+			unless @y + 28 >= 600
+		 	@y +=3
+		 	end
+		end
 end
 
 Game.new.show
